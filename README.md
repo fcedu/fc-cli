dde17d259cefcc835602a01ef03cd9cbe3eb5c26
taozhi <taozhi@inke.cn>
2019-06-19 11:27:49 +0800

    从样板库拉取代码逻辑

--- README.md
+++ README.md
@@ -15,28 +15,28 @@ fc-cli
 <!-- usage -->
 ```sh-session
 $ npm install -g @fcedu/fc-cli
-$ fc COMMAND
+$ fc-cli COMMAND
 running command...
-$ fc (-v|--version|version)
-@fcedu/fc-cli/0.0.0 darwin-x64 node-v10.9.0
-$ fc --help [COMMAND]
+$ fc-cli (-v|--version|version)
+@fcedu/fc-cli/0.0.4 darwin-x64 node-v10.9.0
+$ fc-cli --help [COMMAND]
 USAGE
-  $ fc COMMAND
+  $ fc-cli COMMAND
 ...
 ```
 <!-- usagestop -->
 # Commands
 <!-- commands -->
-* [`fc help [COMMAND]`](#fc-help-command)
-* [`fc init`](#fc-init)
+* [`fc-cli help [COMMAND]`](#fc-cli-help-command)
+* [`fc-cli init`](#fc-cli-init)
 
-## `fc help [COMMAND]`
+## `fc-cli help [COMMAND]`
 
-display help for fc
+display help for fc-cli
 
 ```
 USAGE
-  $ fc help [COMMAND]
+  $ fc-cli help [COMMAND]
 
 ARGUMENTS
   COMMAND  command to show help for
@@ -47,14 +47,14 @@ OPTIONS
 
 _See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v2.2.0/src/commands/help.ts)_
 
-## `fc init`
+## `fc-cli init`
 
 通过选择模板初始化项目。
 
 ```
 USAGE
-  $ fc init
+  $ fc-cli init
 ```
 
-_See code: [src/commands/init.ts](https://github.com/fc/fc-cli/blob/v0.0.0/src/commands/init.ts)_
+_See code: [src/commands/init.ts](https://github.com/fcedu/fc-cli/blob/v0.0.4/src/commands/init.ts)_
 <!-- commandsstop -->
--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,24 +1,78 @@
 import {Command} from '@oclif/command'
 import * as inquirer from 'inquirer'
+import * as shell from 'shelljs'
 
+/**
+ * 可选项目列表
+ */
+const choices: inquirer.objects.ChoiceOption<object>[] = [
+  {
+    name: 'React 不使用 NPM、Node、JSX 的模板项目',
+    value: 'https://github.com/fcedu/react-without-node-boilerplate'
+  }
+]
+
+// 检测 git
+// 选择项目模板
+// 输入项目名称
 
 export default class Init extends Command {
-  static description = '通过选择模板初始化项目。';
+  static description = '通过选择模板初始化项目。'
+
+  async getGitRepository() {
+    const responses: any = await inquirer.prompt([
+      {
+        name: 'gitRepository',
+        message: '请选择一个项目模板!',
+        type: 'list',
+        choices,
+      }
+    ])
+
+    return responses.gitRepository
+  }
+
+  async getProjectName() {
+    const responses: any = await inquirer.prompt([
+      {
+        name: 'projectName',
+        message: '请输入您的项目名称!',
+        type: 'input'
+      }
+    ])
+
+    if (responses.projectName.trim() === '') {
+      let projectName: string = await this.getProjectName()
+
+      return projectName
+    }
+
+    return responses.projectName
+  }
+
+  async cloneBoilerpate(gitRepository: string, projectName: string) {
+    let responses = shell.exec(`git clone ${gitRepository} ${projectName}`)
+
+    return responses.code === 0
+  }
 
   async run() {
+    if (!shell.which('git')) {
+      this.error('该命令依赖 git 环境，请先安装 git!')
+      this.exit(1)
+    }
 
-    let responses: any = await inquirer.prompt([{
-      name: 'project',
-      message: '请选择一个项目模板!',
-      type: 'list',
-      choices: [
-        {name: 'vue 商城项目'},
-        {name: 'react 商城项目'}
-      ],
-    }])
+    let gitRepository: string = await this.getGitRepository()
+    let projectName: string = await this.getProjectName()
 
-    let project = responses.project
+    let ok = await this.cloneBoilerpate(gitRepository, projectName)
 
-    this.log(`您选择的项目是: ${project}`)
+    if (ok) {
+      const path = `${process.cwd()}/${projectName}`
+      shell.cd(path)
+      shell.exec('git remote remove origin')
+    }
+
+    this.log(`创建完成，请切换到 ${ projectName } 下进行相关操作!`)
   }
 }
